3ea0101cbb9a9c8ea12294c80e6d83dfed3f6b93
Victoria Vosipova <crank-line@rambler.ru>
2020-10-11 18:07:36 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsConfigNew = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    console.log(bracketsConfig[i]);
+    bracketsConfigNew.push(bracketsConfig[i].join(''));
+
+  }
+  console.log(bracketsConfigNew);
+
+  for (let j = 0; j < 100; j++) {
+    for (let i = 0; i < bracketsConfigNew.length; i++) {
+      if (str.includes(bracketsConfigNew[i])) {
+        str = str.replace(bracketsConfigNew[i], '');
+      }
+    }
+  }
+  console.log(str.length);
+  console.log(str);
+  return (str.length === 0);
+
 }
+
+
+// your solution
